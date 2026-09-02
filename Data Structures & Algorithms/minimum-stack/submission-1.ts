6b219ebbb1a665149628e1f611cd812f339da9fd
Matheus Werneck <werneckmatheus12@gmail.com>
2026-09-02 09:17:20 -0300

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.ts
@@ -0,0 +1,51 @@
+class MinStack {
+    private stack: Array<number>
+    private sortedValues: Array<number>
+
+
+    constructor() {
+        this.stack = []
+        this.sortedValues = []
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this.stack.push(val)
+
+        if (!this.sortedValues.length)
+            this.sortedValues.push(val)
+            
+        else if (val <= this.sortedValues[this.sortedValues.length - 1])
+                this.sortedValues.push(val)
+        
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        const removed = this.stack.pop()
+
+        if(removed === this.getMin()){
+            this.sortedValues.pop()
+        }
+    }
+
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack[this.stack.length - 1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return this.sortedValues[this.sortedValues.length - 1]
+    }
+}
