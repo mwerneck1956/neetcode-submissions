7d31b46f9bd61d136f8d80ca74b422269395cfca
Matheus Werneck <werneckmatheus12@gmail.com>
2026-09-03 15:29:26 -0300

    Add: count-number-of-islands - submission-3

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.ts
@@ -0,0 +1,60 @@
+class Solution {
+    /**
+     * @param {character[][]} grid
+     * @return {number}
+     */
+    rows: number
+    cols: number
+
+    constructor(){
+        this.rows = 0
+        this.cols = 0
+    }
+
+    numIslands(grid: string[][]): number {
+        this.rows = grid.length
+        this.cols = grid[0].length
+
+        let islands = 0
+
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] != "1") continue
+                
+                
+                islands++
+                this.dfs(i, j, grid)
+            }
+        }
+
+        return islands
+    }
+
+    dfs(elementRow: number, elementCol: number, grid: string[][]) {
+        grid[elementRow][elementCol] = 'X'
+        const stack: Array<{ row: number, col: number }> = [{ row: elementRow, col: elementCol }]
+
+        const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+        while (stack.length) {
+            const neighborElement = stack.pop()
+
+            for (const [row, col] of directions) {
+                const adjacentRow = neighborElement.row + row
+                const adjacentCol = neighborElement.col + col
+
+                if (this.isValidPosition(adjacentRow, adjacentCol) && grid[adjacentRow][adjacentCol] === "1") {
+                    grid[adjacentRow][adjacentCol] = 'X'
+                    stack.push({ row: adjacentRow, col: adjacentCol })
+                }
+            }
+        }
+    }
+
+    isValidPosition(row: number, col: number) {
+        const { rows, cols } = this
+
+        return (row >= 0 && row < rows && col >= 0 && col < cols)
+    }
+}
+
