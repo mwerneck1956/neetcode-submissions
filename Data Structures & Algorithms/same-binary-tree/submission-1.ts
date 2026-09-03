c10ab065999e26def4135a338d3ab8b13303db84
Matheus Werneck <werneckmatheus12@gmail.com>
2026-09-02 21:51:40 -0300

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+        if (!p && !q)
+            return true
+
+        if((!p || !q) || p.val != q.val)
+            return false
+
+        return this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right)
+    }
+}
