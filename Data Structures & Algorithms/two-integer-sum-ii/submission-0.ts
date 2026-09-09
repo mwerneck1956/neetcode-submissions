fdb022f6d3ff5b3ab5218c5739e6b427fe7f95cf
Matheus Werneck <werneckmatheus12@gmail.com>
2026-09-09 10:35:46 -0300

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers: number[], target: number): number[] {
+        let start = 0;
+        let end = numbers.length - 1;
+
+        while(start < end){
+            const sum = numbers[start] + numbers[end]
+
+            if(sum === target)
+                return [start + 1 , end + 1]
+            
+            if(sum > target){
+                end = end - 1
+            }else{
+                start = start + 1
+            }
+        }
+    }
+}
